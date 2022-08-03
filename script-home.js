a92b5ef7dfedefef82695fd705667147f66f9fa0
Abner Andrade <aa.abnerandrade@gmail.com>
2022-08-03 20:00:59 -0300

    Função Adicionar Palavra

--- script-home.js
+++ script-home.js
@@ -1,3 +1,22 @@
+var palavras = ['ORACLE', 'ALURA', 'HTML', 'JAVASCRIPT', 'CSS', 'DESAFIO', 'TRILHA', 'LOGICA', 'SOFTWARE']
+
+function startGame() {
+    console.log(palavras);
+	var palavraSecreta = palavras[Math.floor(Math.random()*palavras.length)];
+	console.log(palavraSecreta);
+	return palavraSecreta;
+}
+
+function salvarNovaPalavra(novaPalavraDigitada) {
+    var palavraDigitada = document.getElementById("novaPalavraDigitada");
+    palavraDigitada = palavraDigitada.value;
+    palavraDigitada = palavraDigitada.toUpperCase();
+    palavras.push(palavraDigitada)
+    console.log(palavras);
+    alert("Palavra inserida!")
+}
+
+document.getElementById("novaPalavraDigitada").focus();
 
 
 
